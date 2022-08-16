28419722aa9dd571cc6fc2d92730560cfa9a1155
r-apt <68184423+r-apt@users.noreply.github.com>
2022-08-16 21:49:38 +0100

    Update raw.lua

--- raw.lua
+++ raw.lua
@@ -191,26 +191,28 @@ end)
 -- Loop
 RunService.RenderStepped:Connect(function()
 	local Humanoid = Player.Character:FindFirstChild('Humanoid')
-	if Humanoid and GodModeEnabled == true then
-			local X, Z = 0, 0
+	if Humanoid then
+			if GodModeEnabled then
+				local X, Z = 0, 0
 
-			if UserInputService:IsKeyDown(Enum.KeyCode.W) then
-				Z += -1
+				if UserInputService:IsKeyDown(Enum.KeyCode.W) then
+					Z += -1
+				end
+
+				if UserInputService:IsKeyDown(Enum.KeyCode.A) then
+					X += -1
+				end
+
+				if UserInputService:IsKeyDown(Enum.KeyCode.S) then
+					Z += 1
+				end
+
+				if UserInputService:IsKeyDown(Enum.KeyCode.D) then
+					X += 1
+				end
+
+				Humanoid:Move(Vector3.new(X, 0, Z), true)	
 			end
-
-			if UserInputService:IsKeyDown(Enum.KeyCode.A) then
-				X += -1
-			end
-
-			if UserInputService:IsKeyDown(Enum.KeyCode.S) then
-				Z += 1
-			end
-
-			if UserInputService:IsKeyDown(Enum.KeyCode.D) then
-				X += 1
-			end
-
-			Humanoid:Move(Vector3.new(X, 0, Z), true)		
 	end
 
 	if AutoBuyEnabled then
